31482414d7b347944a61890b83c030884ae7fe54
BelenGigante <vinn_a@hotmail.com>
2023-01-09 19:01:10 -0500

    starter files

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO department(name);
+VALUES
+('engineering'),
+(''),
+(''),
+(''),
+(''),
+(''),
+INSERT INTO role(title,salary,department_id)
+VALUES
+('lead engineer',100000,1),
+(''),
+(''),
+(''),
+(''),
+(''),
+INSERT INTO employee(first_name,last_name,role_id,manager_id)
+VALUES
+('Jane','Doe',1,1),
+(''),
+(''),
+(''),
+(''),
+(''),
